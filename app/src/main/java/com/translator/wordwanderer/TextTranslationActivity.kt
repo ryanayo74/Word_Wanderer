4f8e4542ad56a894efc18eaddffb670d850f5d7b
ryanayo74 <124416632+ryanayo74@users.noreply.github.com>
2024-05-16 11:01:29 +0800

    Improving translation request

--- app/src/main/java/com/translator/wordwanderer/TextTranslationActivity.kt
+++ app/src/main/java/com/translator/wordwanderer/TextTranslationActivity.kt
@@ -1,16 +1,13 @@
 package com.translator.wordwanderer
 
-import android.app.AlertDialog
-import android.content.ActivityNotFoundException
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.Intent
+import android.content.*
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
-import android.widget.ImageView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,18 +17,20 @@ import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import com.translator.wordwanderer.databinding.ActivityTextTranslationBinding
-import java.util.Locale
+import java.util.*
 
 class TextTranslationActivity : AppCompatActivity() {
-    lateinit var binding: ActivityTextTranslationBinding
+    private lateinit var binding: ActivityTextTranslationBinding
     private lateinit var textToSpeech: TextToSpeech
     private lateinit var progressBar: ProgressBar
 
-    private var items = arrayOf(
-        "English", "Tagalog"
+    private val languages = arrayOf(
+        "Arabic", "Bengali", "Chinese", "Dutch", "English", "French",
+        "German", "Hindi", "Italian", "Japanese", "Korean","Portuguese",
+        "Russian", "Tagalog", "Spanish", "Urdu"
     )
 
-    private var conditions = DownloadConditions.Builder()
+    private val downloadConditions = DownloadConditions.Builder()
         .requireWifi()
         .build()
 
@@ -39,30 +38,24 @@ class TextTranslationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_text_translation)
 
+        setupDropdown(binding.languageFrom, languages)
+        setupDropdown(binding.languageTo, languages)
+
         progressBar = findViewById(R.id.progressBar)
-
-
-        binding.languageFrom.setOnClickListener {
-            showDialog()
-        }
-
-        binding.languageTo.setOnClickListener {
-            showDialog()
-        }
 
         binding.translate.setOnClickListener {
             showLoading(true)
 
             val options = TranslatorOptions.Builder()
-                .setSourceLanguage(selectFrom())
-                .setTargetLanguage(selectTo())
+                .setSourceLanguage(getLanguageCode(binding.languageFrom.text.toString()))
+                .setTargetLanguage(getLanguageCode(binding.languageTo.text.toString()))
                 .build()
 
-            val englishGermanTranslator = Translation.getClient(options)
+            val translator = Translation.getClient(options)
 
-            englishGermanTranslator.downloadModelIfNeeded(conditions)
+            translator.downloadModelIfNeeded(downloadConditions)
                 .addOnSuccessListener {
-                    englishGermanTranslator.translate(binding.input.text.toString())
+                    translator.translate(binding.input.text.toString())
                         .addOnSuccessListener { translatedText ->
                             showLoading(false)
                             binding.output.text = translatedText
@@ -84,23 +77,40 @@ class TextTranslationActivity : AppCompatActivity() {
 
         textToSpeech = TextToSpeech(this) { status ->
             if (status == TextToSpeech.SUCCESS) {
+                // Text-to-Speech engine is initialized successfully
                 val result = textToSpeech.setLanguage(Locale.getDefault())
                 if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    // Language is not supported
                     Toast.makeText(this, "Language is not supported", Toast.LENGTH_LONG).show()
                 }
+            } else {
+                // Text-to-Speech engine initialization failed
+                Toast.makeText(this, "Some TextToSpeech not supported yet", Toast.LENGTH_LONG).show()
             }
         }
 
         binding.sound.setOnClickListener {
-            if (binding.output.text.toString().trim().isNotEmpty()) {
-                textToSpeech.speak(
-                    binding.output.text.toString().trim(),
-                    TextToSpeech.QUEUE_FLUSH,
-                    null,
-                    null
-                )
+            val selectedLanguage = getLanguageCode(binding.languageTo.text.toString())
+            val locale = getLocaleForLanguage(selectedLanguage)
+            val result = textToSpeech.setLanguage(locale)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Language not supported right now", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Required", Toast.LENGTH_LONG).show()
+                val translatedText = binding.output.text.toString().trim()
+                if (translatedText.isNotEmpty()) {
+                    val speakResult = textToSpeech.speak(
+                        translatedText,
+                        TextToSpeech.QUEUE_FLUSH,
+                        null,
+                        null
+                    )
+                    if (speakResult == TextToSpeech.ERROR) {
+                        Log.e("TTS", "Error while speaking")
+                    }
+                } else {
+                    Toast.makeText(this, "Translated text is empty. Please translate some text first.", Toast.LENGTH_LONG).show()
+                }
             }
         }
 
@@ -110,6 +120,15 @@ class TextTranslationActivity : AppCompatActivity() {
 
         binding.copyOutput.setOnClickListener {
             copyToClipboard(binding.output.text.toString())
+        }
+    }
+
+    private fun setupDropdown(autoCompleteTextView: AutoCompleteTextView, items: Array<String>) {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, items)
+        autoCompleteTextView.setAdapter(adapter)
+
+        autoCompleteTextView.setOnClickListener {
+            autoCompleteTextView.showDropDown()
         }
     }
 
@@ -125,48 +144,56 @@ class TextTranslationActivity : AppCompatActivity() {
         }
     }
 
-    private fun selectFrom(): String {
-        return when (binding.languageFrom.text.toString()) {
-            "" -> TranslateLanguage.ENGLISH
+    private fun getLanguageCode(language: String): String {
+        return when (language) {
+            "Arabic" -> TranslateLanguage.ARABIC
+            "Bengali" -> TranslateLanguage.BENGALI
+            "Chinese" -> TranslateLanguage.CHINESE
+            "Dutch" -> TranslateLanguage.DUTCH
             "English" -> TranslateLanguage.ENGLISH
+            "French" -> TranslateLanguage.FRENCH
+            "German" -> TranslateLanguage.GERMAN
+            "Hindi" -> TranslateLanguage.HINDI
+            "Italian" -> TranslateLanguage.ITALIAN
+            "Japanese" -> TranslateLanguage.JAPANESE
+            "Korean" -> TranslateLanguage.KOREAN
+            "Portuguese" -> TranslateLanguage.PORTUGUESE
+            "Russian" -> TranslateLanguage.RUSSIAN
+            "Spanish" -> TranslateLanguage.SPANISH
             "Tagalog" -> TranslateLanguage.TAGALOG
+            "Urdu" -> TranslateLanguage.URDU
             else -> TranslateLanguage.ENGLISH
         }
     }
 
-    private fun selectTo(): String {
-        return when (binding.languageTo.text.toString()) {
-            "" -> TranslateLanguage.ENGLISH
-            "English" -> TranslateLanguage.ENGLISH
-            "Tagalog" -> TranslateLanguage.TAGALOG
-            else -> TranslateLanguage.ENGLISH
+    private fun getLocaleForLanguage(language: String): Locale {
+        return when (language) {
+            TranslateLanguage.ARABIC -> Locale("ar")
+            TranslateLanguage.BENGALI -> Locale("bn")
+            TranslateLanguage.CHINESE -> Locale.SIMPLIFIED_CHINESE
+            TranslateLanguage.ENGLISH -> Locale.ENGLISH
+            TranslateLanguage.FRENCH -> Locale.FRENCH
+            TranslateLanguage.GERMAN -> Locale.GERMAN
+            TranslateLanguage.HINDI -> Locale("hi")
+            TranslateLanguage.ITALIAN -> Locale.ITALIAN
+            TranslateLanguage.JAPANESE -> Locale.JAPANESE
+            TranslateLanguage.KOREAN -> Locale.KOREAN
+            TranslateLanguage.PORTUGUESE -> Locale("pt")
+            TranslateLanguage.RUSSIAN -> Locale("ru")
+            TranslateLanguage.SPANISH -> Locale("es")
+            TranslateLanguage.TAGALOG -> Locale("tl")
+            TranslateLanguage.URDU -> Locale("ur")
+            else -> Locale.ENGLISH
         }
-    }
-
-    private fun showDialog() {
-        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-        builder.setTitle("Register for More Features")
-            .setMessage("To access more features, register now.")
-            .setPositiveButton("Register") { dialog, which ->
-                val intent = Intent(this, RegistrationActivity::class.java)
-                startActivity(intent)
-            }
-            .setNegativeButton("Cancel") { dialog, which ->
-                dialog.dismiss()
-            }
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
-            if (resultCode == RESULT_OK && null != data) {
-                val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                val spokenText = result?.get(0)
-                binding.input.setText(spokenText)
-            }
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK && data != null) {
+            val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val spokenText = result?.get(0)
+            binding.input.setText(spokenText)
         }
     }
 
@@ -178,10 +205,28 @@ class TextTranslationActivity : AppCompatActivity() {
     }
 
     private fun showLoading(show: Boolean) {
-        binding.progressBar.visibility = if (show) View.VISIBLE else View.GONE
+        binding.progressBar.visibility = if (show) {
+            Toast.makeText(this, "Downloading language model. Please wait...", Toast.LENGTH_LONG).show()
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
     }
 
     companion object {
         private const val REQUEST_CODE_SPEECH_INPUT = 100
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        navigateToDashboard()
+    }
+
+    private fun navigateToDashboard() {
+        val username = intent.getStringExtra("Username") // Retrieve username
+        val intent = Intent(this@TextTranslationActivity, DashboardActivity::class.java)
+        intent.putExtra("Username", username) // Pass username to DashboardActivity
+        startActivity(intent)
+        finish()
     }
 }
